b351a665af0daee1f15c9f1cf14974fc5ef3beef
eagrcode <elliot.robinson92@hotmail.com>
2023-03-04 14:52:29 +0000

    add GraphQL api + frontend code

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyappbd0a68b6": {
       "AppClientID": "string",
