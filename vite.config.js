c9f99fc75d40cb8db93e7edb80a964a7d813a3ae
eagrcode <elliot.robinson92@hotmail.com>
2023-03-04 13:24:59 +0000

    add code to vite.config.js to fix vite/aws build fail

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // add resolve below to fix vite/aws build fail
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
 });
